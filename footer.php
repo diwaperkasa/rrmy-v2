738bef25926e1781f82e205b1db90db783191538
diwaperkasa <diwa.perkasa@gmail.com>
2025-12-06 06:56:25 +0700

    update page

--- footer.php
+++ footer.php
@@ -12,18 +12,18 @@
         <footer class="footer bg-black" role="contentinfo">
             <div class="container text-white py-4">
                 <div class="row">
-                    <div class="col-lg-4 border-end border-white">
+                    <div class="col-lg-3 border-end border-white">
                         <div class="border-bottom text-center">
                             <a class="text-decoration-none" href="/">
                                 <img style="max-height: 60px;" class="invert-color mb-4 img-fluid" src="<?= get_stylesheet_directory_uri() . "/assets/images/RobbReport_Malaysia-black-01-V2.webp" ?> ?>" alt="robbreport-malaysia-logo">
                             </a>
                         </div>
                         <div class="my-4">
-                            <p class="lora">Robb Report is leading voice in the global luxury market. Its discerning audience appreciates and desires quality, exclusivity, heritage, taste, and fine design. It is synonomous aith affluence, luxury, and the best of the best.</p>
-                            <p class="text-secondary text-uppercase mulish">ROBB REPORT HONG KONG IS A TRADEMARK OF ROBB REPORT MEDIA, LLC © 2025 ROBB REPORT MEDIA, LLC. ALL RIGHTS RESERVED. PUBLISHED UNDER LICENSE FROM ROBB REPORT MEDIA, LLC, A SUBSIDIARY OF PENSKE MEDIA CORPORATION.</p>
+                            <p class="lora fs-small">Robb Report is leading voice in the global luxury market. Its discerning audience appreciates and desires quality, exclusivity, heritage, taste, and fine design. It is synonomous aith affluence, luxury, and the best of the best.</p>
+                            <p class="text-secondary text-uppercase mulish fs-small">ROBB REPORT HONG KONG IS A TRADEMARK OF ROBB REPORT MEDIA, LLC © 2025 ROBB REPORT MEDIA, LLC. ALL RIGHTS RESERVED. PUBLISHED UNDER LICENSE FROM ROBB REPORT MEDIA, LLC, A SUBSIDIARY OF PENSKE MEDIA CORPORATION.</p>
                         </div>
                     </div>
-                    <div class="col-lg-5">
+                    <div class="col-lg-6">
                         <?php $menus = get_wp_menu_tree('footer'); ?>
                         <div class="row">
                             <?php foreach ($menus as $menu): ?>
