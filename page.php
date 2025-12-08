aa8e331e96d36e017269eba462aee4648d0f62bc
diwaperkasa <diwa.perkasa@gmail.com>
2025-12-08 15:17:42 +0700

    update page

--- page.php
+++ page.php
@@ -2,7 +2,12 @@
 
 <main class="main" role="main">
     <div class="container">
-        <?php the_content(); ?>
+        <div class="py-4 lora">
+            <header class="pb-4">
+                <h1><?= the_title() ?></h1>
+            </header>
+            <?php the_content(); ?>
+        </div>
     </div>
 </main>
 
