0f66198de51651b43b394f1476a79b5ff10fad80
diwaperkasa <diwa.perkasa@gmail.com>
2025-12-09 11:12:09 +0700

    update page

--- page.php
+++ page.php
@@ -1,13 +1,15 @@
 <?php get_header(); ?>
 
 <main class="main bg-light" role="main">
-    <div class="container">
-        <div class="py-4">
-            <header class="pb-4 text-center oranienbaum">
-                <h1 class="mb-0"><?= the_title() ?></h1>
-            </header>
-            <div class="post__content lora">
-                <?php the_content(); ?>
+    <div class="border-bottom">
+        <div class="container">
+            <div class="py-4">
+                <header class="pb-4 text-center oranienbaum">
+                    <h1 class="mb-0"><?= the_title() ?></h1>
+                </header>
+                <div class="post__content lora">
+                    <?php the_content(); ?>
+                </div>
             </div>
         </div>
     </div>
