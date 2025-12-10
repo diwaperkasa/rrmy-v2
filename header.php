5afb2b83d76051df9d518d5af27a2f94d42527b1
diwaperkasa <diwa.perkasa@gmail.com>
2025-12-10 11:02:44 +0700

    update page

--- header.php
+++ header.php
@@ -273,8 +273,8 @@
             <div class="offcanvas-body">
                 <div class="row">
                     <div class="col-md-6 mb-5 mb-md-0">
-                        <h2 class="fw-normal h4 oranienbaum">Newsletters</h2>
-                        <p class="lora">Get Robb Report Malaysia’s take on what matters most in the world of the luxury, delivered to your inbox every week.</p>
+                        <h2 class="fw-normal h4 oranienbaum">Newsletter</h2>
+                        <p class="lora">Sign up for the Robb Report Malaysia weekly newsletter for the latest from the universe of luxury.</p>
                         <form target="_blank" action="https://robbreport.us12.list-manage.com/subscribe/post?u=28729885bbfbb9568a4580c7b&id=6523a24d0a" method="POST">
                             <div class="input-group mb-3">
                                 <input name="EMAIL" type="email" class="fs-small form-control bg-transparent rounded-0 border text-white mulish px-3 py-2" placeholder="EMAIL ADDRESS" aria-label="EMAIL ADDRESS" required>
