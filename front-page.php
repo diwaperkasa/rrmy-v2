9d7ae12434216ca436eda4d5f932829d5b6c74a5
diwaperkasa <diwa.perkasa@gmail.com>
2026-01-30 15:35:58 +0700

    fix subscribe logo

--- front-page.php
+++ front-page.php
@@ -273,7 +273,7 @@
         <div class="container">
             <section id="magazine-subscribe" class="py-5">
                 <div class="text-center px-5">
-                    <img class="img-fluid mb-3" src="<?= get_theme_mod('subscribe_logo' , "https://robbreport.com/wp-content/uploads/2024/10/RR2022_09_shadow.webp"); ?>" alt="<?= get_theme_mod('subscribe_logo' , "https://robbreport.com/wp-content/uploads/2024/10/RR2022_09_shadow.webp"); ?>">
+                    <img class="img-fluid mb-3" src="<?= wp_get_attachment_url( get_theme_mod('subscribe_logo') ); ?>" alt="<?= wp_get_attachment_url( get_theme_mod('subscribe_logo') ); ?>">
                 </div>
                 <h2 class="mb-4 oranienbaum ls-1 text-center">Get The Magazine</h2>
                 <p class="mulish fw-light ls-1">Subscribe now for a one-year subscription at only RM150 for the print version or opt for the digital edition for US$39.90 with two issues free.</p>
