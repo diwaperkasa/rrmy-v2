4c85a3c0fd20237173bcd0b1a000f72d0e6df1f5
diwaperkasa <diwa.perkasa@gmail.com>
2026-03-27 10:52:57 +0700

    update shadow

--- header.php
+++ header.php
@@ -441,7 +441,7 @@ $subscribeLogo = wp_get_attachment_url( get_theme_mod('subscribe_logo') );
             <div class="container">
                 <div class="position-relative subsribe-popup d-none">
                     <div class="position-absolute w-100">
-                        <div class="container bg-white border-top">
+                        <div class="container bg-white border-top shadow">
                             <div class="subsribe-popup-container">
                                 <div class="row">
                                     <div class="col-md-6 py-4">
@@ -484,7 +484,7 @@ $subscribeLogo = wp_get_attachment_url( get_theme_mod('subscribe_logo') );
                     </div>
                 </div>
                 <div class="position-relative search-popup d-none">
-                    <div class="position-absolute w-100 border-top">
+                    <div class="position-absolute w-100 border-top shadow">
                         <div class="container bg-white">
                             <div class="search-popup-container">
                                 <div class="py-5">
